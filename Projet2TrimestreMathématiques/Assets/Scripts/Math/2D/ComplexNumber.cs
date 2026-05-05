bcc424b27e4d77d9151486953fdfd4ecccda5852
GRDF2C <GRDF-IDF-DIRRESEAUX-AI-SLT@grdf.fr>
2026-05-05 13:07:43 +0200

    Matrix 2x2 et complex number

---
+++ Projet2TrimestreMathématiques/Assets/Scripts/Math/2D/ComplexNumber.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Math._2D
+{
+    public class ComplexNumber
+    {
+        public double Real;
+        public double Imaginary;
+
+        // Constructeur
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        // Addition
+        public ComplexNumber Add(ComplexNumber other)
+        {
+            return new ComplexNumber(Real + other.Real, Imaginary + other.Imaginary);
+        }
+
+        // Soustraction
+        public ComplexNumber Subtract(ComplexNumber other)
+        {
+            return new ComplexNumber(Real - other.Real, Imaginary - other.Imaginary);
+        }
+
+        // Multiplication
+        public ComplexNumber Multiply(ComplexNumber other)
+        {
+            double r = Real * other.Real - Imaginary * other.Imaginary;
+            double i = Real * other.Imaginary + Imaginary * other.Real;
+            return new ComplexNumber(r, i);
+        }
+
+        // Division
+        public ComplexNumber Divide(ComplexNumber other)
+        {
+            double denom = other.Real * other.Real + other.Imaginary * other.Imaginary;
+
+            if (denom == 0)
+                throw new DivideByZeroException("Division par un complexe nul");
+
+            double r = (Real * other.Real + Imaginary * other.Imaginary) / denom;
+            double i = (Imaginary * other.Real - Real * other.Imaginary) / denom;
+
+            return new ComplexNumber(r, i);
+        }
+
+        // Conjugaison
+        public ComplexNumber Conjugate()
+        {
+            return new ComplexNumber(Real, -Imaginary);
+        }
+
+        // Module
+        public double Magnitude()
+        {
+            return System.Math.Sqrt(Real * Real + Imaginary * Imaginary);
+        }
+
+        // Argument (angle en radians)
+        public double Argument()
+        {
+            return System.Math.Atan2(Imaginary, Real);
+        }
+
+        // Conversion complexe -> matrice 2x2
+        public Matrix2x2 ToMatrix()
+        {
+            return new Matrix2x2(
+                Real, -Imaginary,
+                Imaginary, Real
+            );
+        }
+
+        // Override ToString (pratique debug)
+        public override string ToString()
+        {
+            return $"{Real} + {Imaginary}i";
+        }
+    }
+}
---
+++ Projet2TrimestreMathématiques/Assets/Scripts/Math/2D/Matrix2x2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Math._2D
+{
+    public class Matrix2x2
+    {
+        public double M11, M12;
+        public double M21, M22;
+
+        // Constructeur
+        public Matrix2x2(double m11, double m12, double m21, double m22)
+        {
+            M11 = m11;
+            M12 = m12;
+            M21 = m21;
+            M22 = m22;
+        }
+
+        // Addition
+        public Matrix2x2 Add(Matrix2x2 other)
+        {
+            return new Matrix2x2(
+                M11 + other.M11, M12 + other.M12,
+                M21 + other.M21, M22 + other.M22
+            );
+        }
+
+        // Multiplication matricielle
+        public Matrix2x2 Multiply(Matrix2x2 other)
+        {
+            return new Matrix2x2(
+                M11 * other.M11 + M12 * other.M21,
+                M11 * other.M12 + M12 * other.M22,
+
+                M21 * other.M11 + M22 * other.M21,
+                M21 * other.M12 + M22 * other.M22
+            );
+        }
+
+        // Déterminant
+        public double Determinant()
+        {
+            return M11 * M22 - M12 * M21;
+        }
+
+        // Inverse
+        public Matrix2x2 Inverse()
+        {
+            double det = Determinant();
+
+            if (det == 0)
+                throw new Exception("Matrice non inversible");
+
+            return new Matrix2x2(
+                M22 / det, -M12 / det,
+                -M21 / det, M11 / det
+            );
+        }
+
+        // Conversion matrice -> complexe
+        public ComplexNumber ToComplex()
+        {
+            // Doit être de la forme :
+            // [ a  -b ]
+            // [ b   a ]
+
+            if (M11 == M22 && M12 == -M21)
+            {
+                return new ComplexNumber(M11, M21);
+            }
+            else
+            {
+                throw new Exception("La matrice ne correspond pas à un nombre complexe valide");
+            }
+        }
+
+        // Override ToString
+        public override string ToString()
+        {
+            return $"[{M11}, {M12}] \n[{M21}, {M22}]";
+        }
+    }
+}
